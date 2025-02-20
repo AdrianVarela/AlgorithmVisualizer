cc08096047cf357b2774fbd6db46386eef96ccc1
adrian <varela.x.adrian@gmail.com>
2025-02-19 20:02:37 -0600

    Add project files.

---
+++ AlgorithmVisualizer/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace AlgorithmVisualizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.chartMain = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.quickSort = new System.Windows.Forms.Button();
+            this.Refresh = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chartMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(197, 415);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(75, 23);
+            this.buttonOK.TabIndex = 0;
+            this.buttonOK.Text = "Bubble Sort";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // chartMain
+            // 
+            this.chartMain.Location = new System.Drawing.Point(12, 12);
+            this.chartMain.Name = "chartMain";
+            this.chartMain.Size = new System.Drawing.Size(776, 397);
+            this.chartMain.TabIndex = 1;
+            this.chartMain.Text = "chart1";
+            this.chartMain.Click += new System.EventHandler(this.chartMain_Click);
+            // 
+            // quickSort
+            // 
+            this.quickSort.Location = new System.Drawing.Point(322, 415);
+            this.quickSort.Name = "quickSort";
+            this.quickSort.Size = new System.Drawing.Size(75, 23);
+            this.quickSort.TabIndex = 2;
+            this.quickSort.Text = "Quick Sort";
+            this.quickSort.UseVisualStyleBackColor = true;
+            this.quickSort.Click += new System.EventHandler(this.quickSort_Click);
+            // 
+            // Refresh
+            // 
+            this.Refresh.Location = new System.Drawing.Point(65, 415);
+            this.Refresh.Name = "Refresh";
+            this.Refresh.Size = new System.Drawing.Size(75, 23);
+            this.Refresh.TabIndex = 3;
+            this.Refresh.Text = "Refresh";
+            this.Refresh.UseVisualStyleBackColor = true;
+            this.Refresh.Click += new System.EventHandler(this.Refresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Refresh);
+            this.Controls.Add(this.quickSort);
+            this.Controls.Add(this.chartMain);
+            this.Controls.Add(this.buttonOK);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chartMain)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartMain;
+        private System.Windows.Forms.Button quickSort;
+        private System.Windows.Forms.Button Refresh;
+    }
+}
+
---
+++ AlgorithmVisualizer/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+
+// A program that visualises different algorithms and their patterns, 
+// using the Windows Forms App (.NET Framework)
+namespace AlgorithmVisualizer
+{
+    public partial class Form1 : Form
+    {
+
+        int goingUp = 11;
+        public Form1()
+        {
+            InitializeComponent();
+
+            Series seriesToAdd = new Series("Data");
+            ChartArea chartAreaMain = new ChartArea("ChartAreaMain");
+            Legend legendMain = new Legend("LegendMain");
+
+            //
+            chartMain.ChartAreas.Add(chartAreaMain);
+            chartMain.Legends.Add(legendMain);
+
+            seriesToAdd.ChartArea = "ChartAreaMain";
+            seriesToAdd.Legend = "LegendMain";
+
+            chartMain.Series.Add(seriesToAdd);
+
+            chartMain.ChartAreas[0].AxisY.Minimum = 0;
+            chartMain.ChartAreas[0].AxisY.Maximum = 100;
+            chartMain.ChartAreas[0].AxisX.Minimum = 0;
+            chartMain.ChartAreas[0].AxisX.Maximum = 100;
+
+            // Create Starting Chart
+            Refresh_Click(null, null);
+
+            //chartMain.ChartAreas.Clear();
+            //chartMain.ChartAreas.Add(seriesToAdd);
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            //chartMain.Series[0].Points.Add(new DataPoint(goingUp, goingUp++));
+
+            for (int i = 0;i < chartMain.Series[0].Points.Count() - 1; i++)
+            {
+                chartMain.Series[0].Points[i].Color = Color.Purple;
+                for(int j = i+1; j < chartMain.Series[0].Points.Count(); j++)
+                {
+                    chartMain.Series[0].Points[j].Color = Color.Pink;
+                    if (chartMain.Series[0].Points[i].YValues[0] > chartMain.Series[0].Points[j].YValues[0])
+                    {
+                        var prev = chartMain.Series[0].Points[i].YValues;
+                        chartMain.Series[0].Points[i].YValues = chartMain.Series[0].Points[j].YValues;
+                        chartMain.Series[0].Points[j].YValues = prev;
+                    }
+                    Refresh();
+                    chartMain.Series[0].Points[j].Color = Color.CornflowerBlue;
+                }
+                chartMain.Series[0].Points[i].Color = Color.CornflowerBlue;
+            }
+            Refresh();
+        }
+
+        private void chartMain_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void quickSort_Click(object sender, EventArgs e)
+        {
+            DataPointCollection easyType = chartMain.Series[0].Points;
+            quickSortHelper(ref easyType, 0, easyType.Count);
+        }
+
+        // A recursive helper method for quicksort
+        private void quickSortHelper(ref DataPointCollection arr, int min, int max)
+        {
+            // Keep Recursing
+            if(max - min > 1)
+            {
+                //int half = ((max - min) / 2) + min;
+
+                // Setting our pivot point, both Value and Position
+                //  Pivot is going to be set at the middle of the array
+                int pivotPos = max - 1;
+                var pivotVal = arr[pivotPos].YValues;
+
+                arr[pivotPos].Color = Color.Red;
+                for(int i = min; i < pivotPos; i++)
+                {
+                    arr[i].Color = Color.Pink;
+                    if (arr[i].YValues[0] > pivotVal[0])
+                    {
+                        // i pos, Yvalue
+                        var other = arr[i].YValues;
+                        // 1 pos before Pivot, Yvalue
+                        var beforePivot = arr[pivotPos - 1].YValues;
+
+                        arr[pivotPos].YValues = other;
+                        arr[i].YValues = beforePivot;
+                        arr[pivotPos - 1].YValues = pivotVal;
+                        pivotPos--;
+                        i--;
+
+                        arr[i+1].Color = Color.CornflowerBlue;
+                        arr[pivotPos + 1].Color = Color.CornflowerBlue;
+                        arr[pivotPos].Color = Color.Red;
+                        Refresh();
+                    }
+                    else
+                    {
+                        Refresh();
+                        arr[i].Color = Color.CornflowerBlue;
+                    }
+                    arr[pivotPos].Color = Color.CornflowerBlue;
+                }
+
+                Refresh();
+                quickSortHelper(ref arr, min, pivotPos);
+                quickSortHelper(ref arr, pivotPos + 1, max);
+            }
+            // Done Recursing
+            else
+            {
+
+            }
+        }
+
+        private void Refresh_Click(object sender, EventArgs e)
+        {
+            chartMain.Series[0].Points.Clear();
+            var rand = new Random();
+            List<int> startList = new List<int>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                startList.Add(i);
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                int nextData = rand.Next(startList.Count);
+                chartMain.Series[0].Points.Add(new DataPoint(i, startList[nextData]));
+                startList.Remove(startList[nextData]);
+            }
+        }
+    }
+}
